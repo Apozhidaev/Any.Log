c1da146df4b7117da9aa1811f8772ea059472c9c
alexanderpo <al.po@outlook.com>
2016-05-05 04:21:52 +0300

    hotfix

--- Ap.Log.Test/Loggers/Files/Arguments.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ap.Logs.Tests.Loggers.Files
-{
-    internal static class Arguments
-    {
-        public static readonly string Root = "root";
-    }
-}
--- Ap.Log.Test/Loggers/Files/Patterns.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ap.Logs.Tests.Loggers.Files
-{
-    internal static class Patterns
-    {
-        public static readonly string Root = "[root]";
-
-        public static readonly string CurrentDirectory = "[cdir]";
-    }
-}
