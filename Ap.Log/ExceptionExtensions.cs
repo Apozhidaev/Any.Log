fa40780d47316671506c36ef890414e77aa60845
alexanderpo <al.po@outlook.com>
2016-05-05 04:29:32 +0300

    hotfix

---
+++ Ap.Log/ExceptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ap.Logs
+{
+    public static class ExceptionExtensions
+    {
+        public static string GetMessage(this Exception e)
+        {
+            var eBase = e.GetBaseException();
+            return string.Format("{1}: {2}{0}StackTrace:{0}{3}", Environment.NewLine, eBase.GetType().Name, eBase.Message, eBase.StackTrace);
+        }
+    }
+}
